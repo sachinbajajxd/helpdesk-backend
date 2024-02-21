b66ff16eb78f6fd569f4c95587e36e195c568818
Sachin <sachinbajaj0001@gmail.com>
2024-02-22 01:17:07 +0530

    Remove unwanted code

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,6 @@ router.get('/', (req, res) => {
 
 router.post('/login', userControllers.Login);
 router.post('/signup', userControllers.Signup);
-// router.get('/tasks/:userId', verifyToken, userControllers.tasks);
-// router.post('/createtask', verifyToken, userControllers.CreateTask);
-// router.delete('/tasks/:id', verifyToken, userControllers.deleteTask);
-// router.put('/tasks/:id', verifyToken, userControllers.updateTask);
 
 
 
